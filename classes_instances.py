9a0b33d231c0fd93874f42b0ca3520758fe59be5
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-05-28 14:17:40 -0400

    update

--- classes_instances.py
+++ classes_instances.py
@@ -1,40 +1,39 @@
-
-
+#Object-oriented programming (OOP) is a method of structuring a program by
+# bundling related properties and behaviors into individual objects
 class Book:
     def __init__(self, title, arthor, price):
         self.title = title
         self.arthor = arthor
         self.price = price
+
         self.location = title[0:2] + "." + arthor[0:2]
 
-    def premuim_book(self):
-        if self.price > 15:
+    def premium_book(self):
+        if self.price > 16:
             return True
         else:
             return False
 
 
-    def foundation(self):
-        return '{}{}'.format(self.title, self.arthor)
+      
 
+book_one = Book("Im going home", "Jace", 50)
+book_two = Book("Hi mom", "Jada", 15)
+print(book_one.title)
+print(book_two.title)
+print(book_one.premium_book())
+print(book_two.premium_book())
+# print(book_one)
+# print(book_two)
 
-    
-
-book_one = Book('Bye Bye', 'Jace', 50)
-book_two = Book('Hi Hie', 'Jada', 10)
-
-# book_one.title = "Going Home"
+# book_one.title = "Im going home"
 # book_one.arthor = "Jace"
 # book_one.price = 50
 
-# book_two.title = "Bye Bye"
-# book_two.arthor = "Jada"  
-# book_two.price = 20
+# print(book_one.title)
 
-print(book_one)
-print(book_two)
+# book_two.title = "hi mom"
+# book_two.arthor = "Jada"
+# book_two.price = 15
 
-print(book_two.title)
-print(book_two.location)
-print(book_two.premuim_book())
-print(book_one.foundation())
+# print(book_two.title)
